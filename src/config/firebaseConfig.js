c37b1ce8cd7657f70f079629a57a1814abba2c99
robi leksono <robileksono@gmail.com>
2025-01-20 16:07:16 +0700

    feat: update default env value

--- src/config/firebaseConfig.js
+++ src/config/firebaseConfig.js
@@ -1,14 +1,17 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from 'firebase/firestore';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const firebaseConfig = {
-    apiKey: process.env.FIREBASE_API_KEY || "AIzaSyBzDBx0i1SdOspiAoTg2R8tWOQOMlnaejU",
-    authDomain: process.env.FIREBASE_AUTH_DOMAIN || "backend-test-d018b.firebaseapp.com",
-    projectId: process.env.FIREBASE_PROJECT_ID || "backend-test-d018b",
-    storageBucket: process.env.FIREBASE_STORAGE_BUCKET || "backend-test-d018b.firebasestorage.app",
-    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID || "33204621668",
-    appId: process.env.FIREBASE_APP_ID || "1:33204621668:web:4a95ee37f2ec6851b62a71",
-    measurementId: process.env.FIREBASE_MEASUREMENT_ID || "G-T4ZWF9PRC2"
+    apiKey: process.env.FIREBASE_API_KEY || "your_api_key",
+    authDomain: process.env.FIREBASE_AUTH_DOMAIN || "your_auth_domain",
+    projectId: process.env.FIREBASE_PROJECT_ID || "your_project_id",
+    storageBucket: process.env.FIREBASE_STORAGE_BUCKET || "your_storage_bucket",
+    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID || "your_messaging_sender_id",
+    appId: process.env.FIREBASE_APP_ID || "your_app_id",
+    measurementId: process.env.FIREBASE_MEASUREMENT_ID || "your_measurement_id"
 };
 
 const app = initializeApp(firebaseConfig);
